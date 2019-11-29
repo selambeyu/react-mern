152cf2aa0549c5e1da877eed29303c0d3f55cf76
melkam beyene <melkam3392beyu@gmail.com>
2019-11-29 08:34:20 -0500

    commit layout

---
+++ client/src/components/layout/footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default ()=>{
+    return (
+        <footer className="bg-dark text-white mt-5 p-4 text-center">
+            Coptyright &copy;{
+                new Date().getFullYear()
+            } DeveConnector
+        </footer>
+    )
+}
