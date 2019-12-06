31258309f191d915b4d820545bc3c8b73061ad09
melkam beyene <melkam3392beyu@gmail.com>
2019-12-05 22:26:58 -0800

    commit link

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,28 @@
 import React from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom'
 // import logo from './logo.svg';
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/footer';
+import Login from './components/auth/login';
+import Register from './components/auth/register';
 import Landing from './components/layout/landing';
 import './App.css';
 
 
 function App() {
   return (
-    <div className="App">
+    <Router>
+      <div className="App">
      <Navbar></Navbar>
-       <Landing></Landing>
+       <Route exact path='/' component={Landing}></Route>
+       <div className="container">
+         <Route exact path='/register' component={Register}></Route>
+         <Route exact pathe='/login' component={Login}></Route>
+         </div>  
        <Footer></Footer>
     </div>
+    </Router>
+    
   );
 }
 
