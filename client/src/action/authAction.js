4a1a21482e166a52a5d52ae920432ab809b781bd
melkam beyene <melkam3392beyu@gmail.com>
2019-12-16 22:29:04 -0800

    commit with router

--- client/src/action/authAction.js
+++ client/src/action/authAction.js
@@ -3,12 +3,14 @@ import axios from 'axios';
 import {GET_ERRORS} from './types'
 
 
-export const  registerUser= userData =>dispatch=>{
+export const  registerUser= (userData,history) =>dispatch=>{
      axios
      .post('http://localhost:4000/users/register',userData)
-     .then(res => console.log(res.data))
+     .then(res => history.push('/login'))
      .catch(err=>dispatch({
          type: GET_ERRORS,
          payload: err.response.data
      }))
-}
+} 
+
+// this.props.history.push('/dashboard')
--- client/src/components/auth/register.js
+++ client/src/components/auth/register.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
+import {withRouter} from 'react-router-dom';
 import {connect} from 'react-redux';
 import {registerUser} from '../../action/authAction'
 
@@ -38,7 +39,7 @@ class Register extends Component{
           password2:this.state.password2
         };
  
-        this.props.registerUser(newUser);
+        this.props.registerUser(newUser,this.props.history);
 
         // console.log(newUser);
        
@@ -111,4 +112,4 @@ const mapStateToProps=(state)=>({
   auth: state.auth,
   errors:state.errors
 })
-export default connect(mapStateToProps,{ registerUser }) (Register);
+export default connect(mapStateToProps,{ registerUser }) (withRouter(Register));
